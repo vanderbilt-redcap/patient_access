f69195bd75eb71a6f1b969bfa64172293e7a2352
Carl Reed <carl.w.reed@vumc.org>
2019-06-12 14:49:05 -0500

    querying db for uploaded icon paths

--- PatientAccess.php
+++ PatientAccess.php
@@ -11,6 +11,17 @@ class PatientAccess extends \ExternalModules\AbstractExternalModule {
 		$dashboardScript = str_replace("CSS_URL", $this->getUrl("css/dash.css"), $dashboardScript);
 		$dashboardScript = str_replace("BOOTSTRAP_URL", $this->getUrl("js/bootstrap.min.js"), $dashboardScript);
 		$dashboardScript = str_replace("DASH_HTML", "`$dashboardHtml`", $dashboardScript);
+		// $dashboardScript = str_replace("ICONPATH", $this->getUrl("resources/001-letter-a.png"), $dashboardScript);
+		
+		// get path to uploaded icon 1
+		$settings = $this->getProjectSettings();
+		// file_put_contents("C:/xampp/htdocs/redcap/modules/patient_access_v0.1/log.txt", print_r($settings, true));
+		$doc_id = $settings["icon_upload"]["value"][0];
+		$sql = "SELECT stored_name FROM redcap_edocs_metadata WHERE doc_id=$doc_id";
+		$result = db_query($sql);
+		$path = db_fetch_assoc($result)["stored_name"];
+		file_put_contents("C:/xampp/htdocs/redcap/modules/patient_access_v0.1/log.txt", print_r("/redcap/edocs/$path", true));
+		$dashboardScript = str_replace("ICONPATH", "/redcap/edocs/$path", $dashboardScript);
 		$js = <<< EOF
 		<script type="text/javascript">
 			$dashboardScript
